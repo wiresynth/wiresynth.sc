811620c0addcc027282652d0871a5cf6b8a41286
Jelly Terra <jellyterra@proton.me>
2025-08-08 14:40:45 +0800

    Assign names from index in order for elements in `Vec`

--- core/src/wiresynth/dsl/Vec.scala
+++ core/src/wiresynth/dsl/Vec.scala
@@ -7,14 +7,15 @@ import scala.reflect.ClassTag
 
 class Vec[T <: Data](val array: Array[T]) extends Data {
   def apply(index: Int): T = array(index)
+
+  array.zipWithIndex.foreach { (data, i) =>
+    data.withMark(s"$i")
+    children += data
+  }
 }
 
 object Vec {
   def fill[T <: Data: ClassTag](n: Int)(elem: => T): Vec[T] = {
-    val vec = Vec(Array.fill(n)(elem))
-    vec.array.zipWithIndex.foreach { (elem, i) =>
-      vec.children += elem.withMark(s"$i")
-    }
-    vec.withMarkDiscarded()
+    Vec(Array.fill(n)(elem))
   }
 }
